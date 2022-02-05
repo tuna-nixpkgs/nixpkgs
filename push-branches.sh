8c57d2eb55a2705c160d97fc041083728ae6c8a0
dramforever <dramforever@live.com>
2022-02-05 21:16:29 +0800

    Add GitHub Actions

---
+++ push-branches.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+BASE_URL="https://mirrors.tuna.tsinghua.edu.cn/nix-channels"
+UPSTREAM_REPO="https://github.com/NixOS/nixpkgs.git"
+MIRROR_REPO="https://github.com/$GITHUB_REPOSITORY"
+
+builtin echo "machine github.com login $GITHUB_ACTOR password $GITHUB_TOKEN"> ~/.netrc
+
+curl -sSL "$BASE_URL/" | sed -Ee '
+    s/.* title="(nix(os|pkgs)-[a-z0-9.-]+)">.*/\1/p
+    d
+' | while IFS= read -r channel; do
+    echo "Updating $channel" >&2
+    git_revision="$(curl -sSL "$BASE_URL/$channel/git-revision")"
+    echo "  -> Git revision = $git_revision" >&2
+
+    git fetch "$UPSTREAM_REPO" "$channel"
+    git push "$MIRROR_REPO" "$git_revision:refs/heads/$channel"
+done
