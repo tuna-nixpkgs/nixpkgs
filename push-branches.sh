05bc1d2d54283bf73ac11e452d2db8300b962f1e
dramforever <dramforever@live.com>
2022-03-05 17:02:56 +0800

    Check before fetching and pushing, faster update

--- push-branches.sh
+++ push-branches.sh
@@ -8,14 +8,30 @@ MIRROR_REPO="https://github.com/$GITHUB_REPOSITORY"
 
 builtin echo "machine github.com login dramforever password $TOKEN_WORKFLOW"> ~/.netrc
 
-curl -sSL "$BASE_URL/" | sed -Ee '
-    s/.* title="(nix(os|pkgs)-[a-z0-9.-]+)">.*/\1/p
-    d
-' | while IFS= read -r channel; do
-    echo "Updating $channel" >&2
-    git_revision="$(curl -sSL "$BASE_URL/$channel/git-revision")"
-    echo "  -> Git revision = $git_revision" >&2
+get_mirror_branches() {
+    curl -sSL "$BASE_URL/" | sed -Ee '
+        s/.* title="(nix(os|pkgs)-[a-z0-9.-]+)">.*/\1/p
+        d
+    ' | while IFS= read -r channel; do
+        git_revision="$(curl -sSL "$BASE_URL/$channel/git-revision")"
+        echo "$git_revision\t$channel"
+    done
+}
 
-    git fetch "$UPSTREAM_REPO" "$channel"
-    git push "$MIRROR_REPO" "$git_revision:refs/heads/$channel"
+get_current_branches() {
+    git ls-remote "$MIRROR_REPO" "refs/heads/*" | sed "s|refs/heads/||g"
+}
+
+join -j2 \
+    <(get_mirror_branches | sort -k1) \
+    <(get_current_branches | sort -k2) \
+    | while IFS=$'\t' read -r channel mirror_rev current_rev; do
+
+    if [ "$mirror_rev" != "$current_rev" ]; then
+        echo "Updating $channel" >&2
+        echo "  $current_rev -> $mirror_rev" >&2
+
+        git fetch "$UPSTREAM_REPO" "$channel"
+        git push "$MIRROR_REPO" "$mirror_rev:refs/heads/$channel"
+    fi
 done
